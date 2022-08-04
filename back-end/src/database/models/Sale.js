57e5dd38c32b80e920846475b9404d81dec3e86b
Rafael Santos <rafaelcs_94@hotmail.com>
2022-08-04 19:26:19 -0300

    refactor: refactoring models

--- back-end/src/database/models/Sale.js
+++ back-end/src/database/models/Sale.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, DataTypes) => {
-  const Sale = sequelize.define('sales', {
+  const Sale = sequelize.define('Sale', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     userId: DataTypes.INTEGER,
     sellerId: DataTypes.INTEGER,
@@ -12,16 +12,17 @@ module.exports = (sequelize, DataTypes) => {
   {
     createdAt: 'saleDate',
     updatedAt: false,
-    underscored: true
+    underscored: true,
+    tableName: 'sales'
   });
 
   Sale.associate = (models) => {
     Sale.belongsTo(models.User, {
-      as: 'users',
+      as: 'userClient',
       foreignKey: 'userId',
     });
     Sale.belongsTo(models.User, {
-      as: 'users',
+      as: 'userSeller',
       foreignKey: 'sellerId',
     });
   };
