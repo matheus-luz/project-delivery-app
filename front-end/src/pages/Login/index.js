4af67a50df12867261fe6bc03fedd22b3ac57a6e
Tiago Abranges <tiagoabranges@gmail.com>
2022-08-04 16:45:17 -0300

    :bug: lint

--- front-end/src/pages/Login/index.js
+++ front-end/src/pages/Login/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 function Login() {
   return (
     <div>
       <h1>Login</h1>
       <a href="/register">
-      <input type="button" value="Botão de cadastro"/>
+        <input type="button" value="Botão de cadastro" />
       </a>
     </div>
   );
