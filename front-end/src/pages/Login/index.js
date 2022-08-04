540b877e1b2624c7c7270816346caada44830cbc
Tiago Abranges <tiagoabranges@gmail.com>
2022-08-04 16:40:45 -0300

    feat: registration button on login screen

--- front-end/src/pages/Login/index.js
+++ front-end/src/pages/Login/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div>
+      <h1>Login</h1>
+      <a href="/register">
+      <input type="button" value="Botão de cadastro"/>
+      </a>
+    </div>
+  );
 }
 
 export default Login;
