8407d012a0aa81fe929bc76b89d2669b1c4b9a42
Gabriel Rodrigues <huijirohankei@gmail.com>
2022-08-04 15:54:33 -0300

    feat(frontend): Feito setup de rotas do frontend

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <Home /> } />
+          <Route path="/login" element={ <Login /> } />
+          <Route path="/register" element={ <Register /> } />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
