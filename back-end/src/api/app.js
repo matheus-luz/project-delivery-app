8c2fda78af4978401580842beb34164ebe1a009e
Rafael Santos <rafaelcs_94@hotmail.com>
2022-08-05 17:09:38 -0300

    feat: adds use of cors

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const error = require('../middlewares/error');
 const LoginRouter = require('../routes/Login');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/login', LoginRouter);
