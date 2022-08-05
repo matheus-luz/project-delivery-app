fdf727751899154d7d688a1c06af03ee20da954e
Laurenz Pinder <laurenzdpinder90@gmail.com>
2022-08-05 12:22:06 -0300

    fix: eslint

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const LoginRouter = require('../routes/Login');
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-app.use('/login', LoginRouter)
+app.use('/login', LoginRouter);
 
 module.exports = app;
