d0924a48610b7ff92a8c61761ee7205eb85034d7
Laurenz Pinder <laurenzdpinder90@gmail.com>
2022-08-05 15:48:00 -0300

    feat: add customError

--- back-end/src/middlewares/error.js
+++ back-end/src/middlewares/error.js
@@ -1,5 +1,5 @@
 module.exports = (error, _req, res, _next) => {
-  // if (error.status) return res.status(error.status).json({ message: error.message });
+  if (error.status) return res.status(error.status).json({ message: error.message });
   console.log(error.message);
   return res.status(500).json({ message: 'Somenthing went wrong here, please try again later' });
 };
--- back-end/src/services/Login.js
+++ back-end/src/services/Login.js
@@ -1,13 +1,15 @@
 const md5 = require('md5');
 const { User } = require('../database/models');
+const customError = require('../utils/customError');
 
 module.exports = async (body) => {
   const { email, password } = body;
   const user = await User.findOne({ where: { email } });
-  console.log(password);
-  console.log(md5(password));
+  // console.log(md5(password));
 
-  // if (!user) throw errorFunction(BAD_REQUEST, 'Invalid fields');
+  if (!user) throw new customError(404, 'Not found');
+
+  if (md5(password) !== user.password) throw new customError(401, 'Unauthorized');
 
   return user;
 };
