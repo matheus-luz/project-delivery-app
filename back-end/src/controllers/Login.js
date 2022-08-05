177b2793374724dc70a85cfb8fe154d01c5427d0
Rafael Santos <rafaelcs_94@hotmail.com>
2022-08-05 19:17:57 -0300

    refactor: return of /login POST method

--- back-end/src/controllers/Login.js
+++ back-end/src/controllers/Login.js
@@ -1,8 +1,16 @@
 const LoginService = require('../services/Login');
+const generateToken = require('../utils/generateToken');
 
 module.exports = async (req, res) => {
   const { body } = req;
   const user = await LoginService(body);
 
-  return res.status(200).json(user);
+  const token = generateToken(user.id, user.email);
+
+  return res.status(200).json({ 
+    name: user.name,
+    email: user.email,
+    role: user.role,
+    token,
+  });
 };
