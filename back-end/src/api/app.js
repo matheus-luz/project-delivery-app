f3ca30c43dced15be94f75788f7394da09160204
Laurenz Pinder <laurenzdpinder90@gmail.com>
2022-08-05 14:33:27 -0300

    feat: add error middleware

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const error = require('../middlewares/error');
 const LoginRouter = require('../routes/Login');
 
 const app = express();
 app.use(express.json());
 
 app.use('/login', LoginRouter);
+app.use(error);
 
 module.exports = app;
