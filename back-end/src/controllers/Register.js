8f62cb4e36a258140f375b25b34b2401bee6e422
Rafael Santos <rafaelcs_94@hotmail.com>
2022-08-08 18:40:25 -0300

    refactor: token now saves user's role in its data instead of email

--- back-end/src/controllers/Register.js
+++ back-end/src/controllers/Register.js
@@ -5,9 +5,10 @@ module.exports = async (req, res) => {
   const { body } = req;
   const user = await RegisterService(body);
 
-  const token = generateToken(user.id, user.email);
+  const token = generateToken(user.id, user.role);
 
-  return res.status(201).json({ 
+  return res.status(201).json({
+    id: user.id,
     name: user.name,
     email: user.email,
     role: user.role,
