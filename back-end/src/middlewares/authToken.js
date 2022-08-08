722b6f82cd8dd53b3a76e3bb5c844dd9d0260282
Rafael Santos <rafaelcs_94@hotmail.com>
2022-08-08 20:06:31 -0300

    fix: error handling

--- back-end/src/middlewares/authToken.js
+++ back-end/src/middlewares/authToken.js
@@ -7,7 +7,7 @@ const JWT_SECRET = fs.readFileSync('./jwt.evaluation.key', { encoding: 'utf8' })
 module.exports = async (req, _res, next) => {
   const token = req.headers.authorization;
 
-  if (!token) throw new CustomError(401, 'Token not found');
+  if (!token) next(new CustomError(401, 'Token not found'));
 
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
