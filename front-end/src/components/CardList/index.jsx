2d73090e7bcc43fb8c225fc90885db973336ba4c
Tiago Abranges <tiagoabranges@gmail.com>
2022-08-08 19:32:52 -0300

    :rocket: req 12

--- front-end/src/components/CardList/index.jsx
+++ front-end/src/components/CardList/index.jsx
@@ -3,7 +3,6 @@ import productsMock from '../../helpers/productsMock';
 import CardProduct from '../CardProduct';
 
 function CardProducts() {
-  // const [products, setProducts] = useState([]);
   const data = productsMock;
   console.log({ data });
   return (
@@ -12,7 +11,13 @@ function CardProducts() {
       {
         data.map((product) => <CardProduct key={ product.id } product={ product } />)
       }
-      <button type="button">Ver Carrinho</button>
+      <button
+        type="button"
+        data-testid="customer_products__checkout-bottom-value"
+      >
+        Ver Carrinho
+
+      </button>
     </>
   );
 }
