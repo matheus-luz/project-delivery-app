e17e310b3e88aef3ddd832a770a61ed80167c813
Rafael Santos <rafaelcs_94@hotmail.com>
2022-08-10 11:03:43 -0300

    refactor: adds valid values for 'role'

--- back-end/src/middlewares/validateAdminRegister.js
+++ back-end/src/middlewares/validateAdminRegister.js
@@ -5,7 +5,7 @@ const schema = Joi.object({
   name: Joi.string().min(12).required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
-  role: Joi.string().required(),
+  role: Joi.string().valid('administrator', 'seller', 'customer').required(),
 });
 
 module.exports = (req, _res, next) => {
