892c2243229a1e506487543e128d47500304b631
Tiago Abranges <tiagoabranges@gmail.com>
2022-08-10 19:47:22 -0300

    :sparkles: esqueleto checkout pronto

--- front-end/src/pages/Checkout/index.jsx
+++ front-end/src/pages/Checkout/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import AdressDetails from '../../components/checkout/AdressDetails';
+import FinalizarPedido from '../../components/checkout/FinalizarPedido';
 
 function Checkout() {
   return (
     <div>
+      <FinalizarPedido />
       <AdressDetails />
     </div>
   );
