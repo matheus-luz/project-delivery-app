7940bb883a5fdb0c8e2698515424268c4bd814c5
Rafael Santos <rafaelcs_94@hotmail.com>
2022-08-10 10:45:53 -0300

    refactor: defines a setter on User model to cryptograph password

--- back-end/src/database/models/User.js
+++ back-end/src/database/models/User.js
@@ -1,9 +1,14 @@
+const md5 = require('md5');
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     name: DataTypes.STRING,
     email: DataTypes.STRING,
-    password: DataTypes.STRING,
+    password: {
+      type: DataTypes.STRING,
+      set(value) { this.setDataValue('password', md5(value)) }
+    },
     role: {
       type: DataTypes.STRING,
       defaultValue: 'customer'
