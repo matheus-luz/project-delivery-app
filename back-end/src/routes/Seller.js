82b81b692797ad4c0121a1d3612380261eba76f5
Laurenz Pinder <laurenzdpinder90@gmail.com>
2022-08-11 15:28:13 -0300

    refactor: add validateStatusUpdate to endpoint PATCH seller/orders/update/:id

--- back-end/src/routes/Seller.js
+++ back-end/src/routes/Seller.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const rescue = require('express-rescue');
 const SellerController = require('../controllers/Seller');
 const authToken = require('../middlewares/authToken');
+const validateStatusUpdate = require('../middlewares/validateStatusUpdate');
 
 router.get('/orders', rescue(SellerController.getOrders));
 router.get('/orders/:id', authToken, rescue(SellerController.findById));
-router.patch('/orders/update/:id', authToken, rescue(SellerController.update));
+router
+  .patch('/orders/update/:id', authToken, validateStatusUpdate, rescue(SellerController.update));
 
 module.exports = router;
