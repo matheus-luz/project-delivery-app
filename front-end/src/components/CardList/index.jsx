88dbff60cc3c9fcd5f785bb6169450ca7e36cd3b
Gabriel Rodrigues <huijirohankei@gmail.com>
2022-08-11 17:53:16 -0300

    feat(css): Feito visual da página de produtos.

--- front-end/src/components/CardList/index.jsx
+++ front-end/src/components/CardList/index.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
-import productsMock from '../../helpers/productsMock';
+import React, { useContext, useEffect, useState } from 'react';
+import { userContext } from '../../context/userContext';
 import CardProduct from '../CardProduct';
 
 function CardProducts() {
-  const data = productsMock;
+  const [products, setProducts] = useState();
+  const { user } = useContext(userContext);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('/api/customer/products', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          authorization: user.token,
+        },
+      });
+      const json = await response.json();
+      setProducts(json);
+    }
+    fetchData();
+  }, [user]);
+
   return (
-    <>
-      {
-        data.map((product) => <CardProduct key={ product.id } product={ product } />)
-      }
-    </>
+    <div className="flex flex-wrap gap-3 m-3">
+      {products && products.map((product) => (
+        <CardProduct key={ product.id } product={ product } />
+      ))}
+    </div>
   );
 }
 
--- front-end/src/components/Counter/index.jsx
+++ front-end/src/components/Counter/index.jsx
@@ -7,25 +7,28 @@ function Counter({ product }) {
   return (
     <div>
       <button
+        className="bg-trybe-primary text-lg px-2 text-white rounded-l-lg"
         data-testid={ ` customer_products__button-card-rm-item-${product.id}` }
         type="button"
         onClick={ (() => setCounter(counter - 1)) }
       >
         -
-
       </button>
       <input
+        className="text-center text-lg"
+        min={ 0 }
         data-testid={ `customer_products__input-card-quantity-${product.id} ` }
         type="text"
+        readOnly
         value={ counter }
       />
       <button
+        className="bg-trybe-primary text-lg px-2 text-white rounded-r-lg"
         data-testid={ `customer_products__button-card-add-item-${product.id} ` }
         type="button"
         onClick={ (() => setCounter(counter + 1)) }
       >
         +
-
       </button>
     </div>
   );
