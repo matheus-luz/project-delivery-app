f2a2eb8367c36bfbcebb90e6e3fbb22a133a8e3f
Laurenz Pinder <laurenzdpinder90@gmail.com>
2022-08-11 15:48:52 -0300

    refactor: remove sellerError utils

--- back-end/src/controllers/Seller.js
+++ back-end/src/controllers/Seller.js
@@ -1,11 +1,11 @@
 const service = require('../services/Seller');
-const SallerError = require('../utils/sellerError');
+const CustomError = require('../utils/customError');
 
 const update = async (req, res) => {
   const { id } = req.params;
   const { status } = req.body;
 
-  if (!status) throw new SallerError(400, 'Status does not exist');
+  if (!status) throw new CustomError(400, 'Status does not exist');
 
   const { statusCode, data } = await service.update(id, status);
 
