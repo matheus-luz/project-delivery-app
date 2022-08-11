df73f68a1a57b68984dba0b1085de99d028c3ede
Laurenz Pinder <laurenzdpinder90@gmail.com>
2022-08-11 15:42:02 -0300

    refactor: add endpoints GET /orders & orders/:id

---
+++ back-end/src/controllers/Order.js
@@ -0,0 +1,20 @@
+const OrderService = require('../services/Order');
+
+const readOrders = async (req, res) => {
+  const { user } = req;
+  console.log(user);
+  const formatedOrders = await OrderService.readOrders(user);
+
+  return res.status(200).json(formatedOrders);
+};
+
+const readOrderbyId = async (req, res) => {
+  const { id } = req.params;
+  const order = await OrderService.readOrderbyId(id);
+  return res.status(200).json(order);
+};
+
+module.exports = {
+  readOrders,
+  readOrderbyId,
+};
