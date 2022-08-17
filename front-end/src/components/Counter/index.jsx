92edac9a81354d3f96cc46518ee45c7064d66568
Gabriel Rodrigues <huijirohankei@gmail.com>
2022-08-17 17:01:29 -0300

    fix(counter): Adicionado manipulação direta

--- front-end/src/components/Counter/index.jsx
+++ front-end/src/components/Counter/index.jsx
@@ -1,18 +1,20 @@
 import PropTypes from 'prop-types';
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { CartContext } from '../../context/cartContext';
 
 function Counter({ product }) {
   const { cart, setCart } = useContext(CartContext);
 
-  const handleCounter = (operation) => {
+  const handleCounter = (operation, quantity) => {
     const products = [...cart];
     const i = products.findIndex((item) => item.id === product.id);
 
     if (operation === 'increment') {
       products[i].quantity += 1;
-    } else if (products[i].quantity > 0) {
-      products[i].quantity -= 1;
+    } else if (operation === 'decrement') {
+      if (products[i].quantity > 0) products[i].quantity -= 1;
+    } else if (operation === 'set') {
+      products[i].quantity = quantity;
     }
 
     setCart(products);
@@ -24,16 +26,16 @@ function Counter({ product }) {
         className="bg-trybe-primary text-lg px-2 text-white rounded-l-lg"
         data-testid={ `customer_products__button-card-rm-item-${product.id}` }
         type="button"
-        onClick={ (() => handleCounter('decrease')) }
+        onClick={ (() => handleCounter('decrement')) }
       >
         -
       </button>
       <input
         className="text-center text-lg"
         data-testid={ `customer_products__input-card-quantity-${product.id}` }
-        readOnly
-        type="text"
+        type="number"
         value={ product.quantity }
+        onChange={ ((e) => handleCounter('set', e.target.value)) }
       />
       <button
         className="bg-trybe-primary text-lg px-2 text-white rounded-r-lg"
