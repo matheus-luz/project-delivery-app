fec5fa7e23b0e3d7dc379e4341a3a7f7e3e642f5
Rafael Santos <rafaelcs_94@hotmail.com>
2022-08-17 16:58:22 -0300

    feat: adds details pages for customer and seller

---
+++ front-end/src/pages/SellerDetails/index.js
@@ -0,0 +1,20 @@
+import Header from '../../components/Header';
+import HeaderButton from '../../components/Header/HeaderButton';
+import OrderDetails from '../../components/OrderDetails';
+
+function SellerDetails() {
+  return (
+    <>
+      <Header>
+        <HeaderButton
+          to="/seller/orders"
+          label="PEDIDOS"
+          testid="customer_products__element-navbar-link-products"
+        />
+      </Header>
+      <OrderDetails userRole="seller" />
+    </>
+  );
+}
+
+export default SellerDetails;
