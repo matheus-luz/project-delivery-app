d4312db6a1d2c6e87ccc61cfc0878fb371dd5074
Laurenz Pinder <laurenzdpinder90@gmail.com>
2022-08-17 13:00:26 -0300

    feat: start adressDetail implementation

--- front-end/src/components/checkout/AdressDetails.jsx
+++ front-end/src/components/checkout/AdressDetails.jsx
@@ -1,17 +1,41 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+import { userContext } from '../../context/userContext';
 import Button from '../Library/Button';
+import getTotalPrice from '../../utils/totalPrice';
 import TextInput from '../Library/TextInput';
+import { getProducts } from '../../utils/localStorage';
 
 function AdressDetails() {
-  const navigate = useNavigate();
+  const { user } = useContext(userContext);
+  // const navigate = useNavigate();
 
   const [address, setAdress] = useState('');
   const [number, setNumber] = useState('');
 
-  const handleClick = () => {
-    navigate('/customer/orders/:1', { replace: true });
-  }; // id é retornado da requisição da venda; precisa pega-lo ainda
+  const productsData = getProducts();
+  const products = productsData.map(({ id, quantity }) => ({ productId: id, quantity }));
+
+  const handleClick = async () => {
+    await fetch('/api/customer/checkout', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: user.token,
+      },
+      body: JSON.stringify(
+        {
+          sellerId: 2,
+          totalPrice: getTotalPrice(productsData),
+          deliveryAddress: address,
+          deliveryNumber: number,
+          products,
+        },
+      ),
+    });
+    // Falta implementar o redirecionamente da rota e o trycatch do POST
+  };
+
   return (
     <div>
       <h3>Detalhes e Endereço para Entrega</h3>
@@ -29,27 +53,27 @@ function AdressDetails() {
         </select>
       </label>
       <TextInput
-        placeholder="Numero"
-        data-testid="customer_checkout__input-addressNumber"
-        type="number"
-        id="user-numero"
-        label="Numero"
-        value={ number }
-        onChange={ (e) => setNumber(e.target.value) }
-      />
-      <TextInput
         placeholder="Digite o endereço"
         data-testid="customer_checkout__input-address"
         type="text"
         id="user-name"
-        label="Name"
+        label="Endereço"
         value={ address }
         onChange={ (e) => setAdress(e.target.value) }
       />
+      <TextInput
+        data-testid="customer_checkout__input-addressNumber"
+        id="user-numero"
+        label="Número"
+        onChange={ (e) => setNumber(e.target.value) }
+        placeholder="Numero"
+        type="number"
+        value={ number }
+      />
       <Button
         onClick={ handleClick }
-        type="button"
         testid="customer_checkout__button-submit-order"
+        type="button"
       >
         Finalizar Pedido
       </Button>
